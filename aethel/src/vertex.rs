d87dfd0aaeb0ce07e27bbaa75f4a3d0a119bc4c3
haiminh256 <haiminh2022012@gmail.com>
2026-07-18 21:39:30 +0700

    Vertex struct

--- aethel/src/vertex.rs
+++ aethel/src/vertex.rs
@@ -7,12 +7,12 @@ use nalgebra_glm as glm;
 #[repr(C)]
 pub struct Vertex {
     position: glm::Vec3,
-    color: glm::Vec3,
+    color: glm::Vec4,
     normal: glm::Vec3,
     tex_coord: glm::Vec2
 }
 impl Vertex {
     pub fn new(position: glm::Vec3, color: glm::Vec3, normal: glm::Vec3, tex_coord: glm::Vec2) -> Self {
-        Self { position, color, normal, tex_coord }
+        Self { position, color: glm::vec4(color.x, color.y, color.z, 1.0), normal, tex_coord }
     }
 }
