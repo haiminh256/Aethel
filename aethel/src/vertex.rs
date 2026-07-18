1d4e63edace58fd8e60d6d57568e089d3a717df4
haiminh256 <haiminh2022012@gmail.com>
2026-07-18 21:16:36 +0700

    Vertex struct

--- aethel/src/vertex.rs
+++ aethel/src/vertex.rs
@@ -1,3 +1,18 @@
 pub mod vertexbuffer;
 pub mod elementbuffer;
 pub mod arraybuffer;
+
+use nalgebra_glm as glm;
+#[allow(dead_code)]
+#[repr(C)]
+pub struct Vertex {
+    position: glm::Vec3,
+    color: glm::Vec3,
+    normal: glm::Vec3,
+    tex_coord: glm::Vec2
+}
+impl Vertex {
+    pub fn new(position: glm::Vec3, color: glm::Vec3, normal: glm::Vec3, tex_coord: glm::Vec2) -> Self {
+        Self { position, color, normal, tex_coord }
+    }
+}
